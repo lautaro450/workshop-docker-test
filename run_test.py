3755cd734fb3b7b2016eb14eba7369952ef1c4ca
Dante Perea <lautaro.arg@gmail.com>
2019-07-13 13:10:44 +0800

    test -1

--- run_test.py
+++ run_test.py
@@ -6,6 +6,8 @@ class TestRun(unittest.TestCase):
 
     def test_execute(self):
       self.assertEqual(2, run.execute(1))
+    def test_execute_minus(self):
+      self.assertEqual(0, run.execute(-1))
 
 if __name__ == '__main__':
     unittest.main()
